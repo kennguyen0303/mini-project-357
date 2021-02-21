7b7abf82e2316dca1f55dfcc18e1366d6093b15e
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-02-20 20:52:17 -0500

    Update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Navbar,Nav } from 'react-bootstrap';
+import logo from "../../resources/logo/logo.png"
 
 
 const NavBar=()=> {
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home" className="">
+            <img
+                src={logo}
+                width="100"
+                height="60"
+                className=""
+                alt="Optiscreen logo"
+              />
+              </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
+              <Nav className="">
                 <Nav.Link href="#problem">Problem</Nav.Link>
+                <Nav.Link href="#research">Research</Nav.Link>
+                <Nav.Link href="#research">Research</Nav.Link>
                 <Nav.Link href="#research">Research</Nav.Link>
               </Nav>
               <Nav>
--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,57 +1,148 @@
 import React, { Component } from 'react'
-import {Carousel } from 'react-bootstrap';
+import {Carousel,Table } from 'react-bootstrap';
 import screenHourImg from '../resources/graphs/screen_hour.PNG'
 import covid_impactImg from '../resources/graphs/covid_impact.PNG'
 import tooOftenUsedImg from '../resources/graphs/too_often_used.PNG'
+import interviewer_ageImg from '../resources/graphs/interviewer_age.PNG'
+import interviewer_statusImg from '../resources/graphs/interviewer_status.PNG'
+import mostly_used_devicesImg from '../resources/graphs/mostly_used_devices.PNG'
+import often_appImg from '../resources/graphs/often_app.PNG'
+import reminder_responseImg from '../resources/graphs/reminder_response.PNG'
+import rewardImg from '../resources/graphs/reward.PNG'
 
 
 const Research=()=> {
         return (
-            <div className="a_page page_2" id="research">
-            <h1>II. Research </h1>
-            <div  className="text-container">
-                <h2>1. Survey</h2>
-                <p>
-                  We have surveyed <strong>38</strong> people and found that Covid-19 has affected their lives.
-                  Our participants are mostly university students and workers who must switch to
-                  work or study remotely. 
-                </p>
-            </div>
-            <Carousel pause="hover" className="carousel">
-              <Carousel.Item interval={1000}>
-                <img
-                  className="d-block w-75"
-                  src={screenHourImg}
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>HELLO so as you can see this one very bad</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item interval={500}>
-                <img
-                  className="d-block w-75"
-                  src={covid_impactImg}
-                  alt="Second slide"
-                />
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-75"
-                  src={tooOftenUsedImg}
-                  alt="Third slide"
-                />
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+            <div className="a_page" id="research">
+              <h1>II. Research </h1>
+              <div  className="text-container">
+                  <h2>1. Survey</h2>
+                  <h4>A. Method</h4>
+                  <p>
+                  In order to collect the information regarding the users’ 
+                  screen habit, we have run a survey on Google Form and shared 
+                  it on our social network (Facebook) and the class 
+                  communicating protocols including Slack and Discord. 
+                  After two days running the form, we received 38 answers. 
+                  Based on these answers, our hypothesis was confirmed and 
+                  the needs of the users were established. 
+                  </p>
+                  <h4>B. Our analysis</h4>
+                  <p>
+                  1. Most of the participants were students and workers who must 
+                  switch to study and work remotely. Thus, they found Covid-19 
+                  has an impact on their screen habit. (Graphs: age, employment 
+                  status and covid impact)
+                  </p>
+                  <p>
+                  2. The majority spend from 6 to 10 hours working on their smartphones or laptops for 
+                  their school / work per day. An increase by 30-50 hours on screen per week. 
+                  (Apps and hours used often)
+                  </p>
+                  <p>
+                  3. Most people regret spending too much time than wanted on social 
+                  networks or streaming Apps (such as Youtube). (Apps used too often)
+                  </p>
+              </div>
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={screenHourImg}
+                    alt="First slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={covid_impactImg}
+                    alt="Second slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={tooOftenUsedImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={interviewer_ageImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={interviewer_statusImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={mostly_used_devicesImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={often_appImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={reminder_responseImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-75"
+                    src={rewardImg}
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+              </Carousel>
+              <br/>
+              <div  className="text-container">
+                  <h2>2. Research on related Apps</h2>
+                  <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>App's name</th>
+                      {Array.from({ length: 12 }).map((_, index) => (
+                        <th key={index}></th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                      ))}
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                      ))}
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
           </div>
         )
 }
