7cc1c42f987eb6309e08d18a98d8d0188b4e9fce
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-02-20 23:27:48 -0500

    re-enumerate

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -29,44 +29,44 @@ const Design=()=> {
                     <img src={sketch1} height="" width="75%"/>
                     <br></br>
                     <img src={sketch2} width="75%"/>
-                <h2>3. Wireframe</h2>
+                <h2>2. Wireframe</h2>
                     <p>
                     After we finish sketching, we start making wireframe to know what are the
                     necessary components 
                     </p>
                     <img src={wireframe} />
-                <h2>2. Color Palette</h2>
+                <h2>3. Color Palette</h2>
                     <p>
                     Before we jump into final design, we have to decide what are the main color themes for our application.We chose Black, Pink and White as the color for our mock-ups design since those colors bring our user a positive healthy vibe.
                     </p>
                     <img src={color}/>
                     
-                <h2>3. Font</h2>
+                <h2>4. Font</h2>
                     <p>
                     To match our minimalistic style of our app, we decided to choose Inter - a variable font family carefully crafted and designed for the computer screen. 
                     Inter is considered the most common text font in almost all iOS mobile apps. We will take advantage of it so that users can have a pleasant experience. 
                     </p>
                     <img src={font}/>
-                <h2>4. Final Design</h2>
+                <h2>5. Final Design</h2>
                     <p>
                     With chosen color palette and text font, we are finally able to create our final design. 
                     </p>
-                    <h3>4.1 Login/Signup</h3>
+                    <h3>5.1 Login/Signup</h3>
                         <img src={begin}/>
                         <img src={Login}/>
                         <img src={Signup}/>
-                    <h3>4.2 Social Media</h3>
+                    <h3>5.2 Social Media</h3>
                         <img src={Community}/>
-                    <h3>4.3 Habit Tracking</h3>
+                    <h3>5.3 Habit Tracking</h3>
                         <img src={habit}/>
                         <img src={habit1}/>
                         <img src={habit2}/>
-                    <h3>4.4 Knowledge Resource</h3>
+                    <h3>5.4 Knowledge Resource</h3>
                         <img src={Learn}/>  
                         <img src={Sleep}/>  
-                    <h3>4.5 Profile</h3>
+                    <h3>5.5 Profile</h3>
                         <img src={Profile}/>
-                <h2>5. Demo</h2>
+                <h2>6. Demo</h2>
                 <p>The video below demonstrates the design of our application</p>
                 <video width="750" height="500" controls id="demo">
                     <source src={Prototype} type="video/mp4"/>
