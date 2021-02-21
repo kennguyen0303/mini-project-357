25240a5d74307cc6c661ebc0bf261105eb572df7
Tung Leu <leudeptrai@gmail.com>
2021-02-20 22:44:30 -0500

    added conclusion

---
+++ src/pages/Conclusion.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+const Conclusion=()=> {
+        return (
+            <div className="a_page" id="">
+           <h1>Conclusion</h1>
+           <h3>What we learned</h3>
+           <p>This project was such a rewarding challenge for us. We think that the hardest part is to understand the current situation and analyze the user's need.
+               Afer the research, we were able to know how COVID 19 has affected to people's daily lifestyle and offer a solution that can help people cope with it.
+
+           </p>
+           <h3>Next Steps</h3>
+           <p>- Develop a business model so that we can extend the application for more audience</p>
+           <br/>
+           <p>- Research on how to reward user more accurately</p>
+           <br/>
+           <p>- Review our prototype with real user so we can enhance our user expererience</p>
+
+          </div>
+        )
+}
+
+export default Conclusion
